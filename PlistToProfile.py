4d5cb217af18293d881ac7015cd15ae6191b940f
Timothy Sutton <tim@synthist.net>
2012-06-20 16:32:39 -0400

    Initial commit

---
+++ PlistToProfile.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+# PlistToProfile.py
+# Simple utility to assist with creating Profiles from generic plists
+
+import sys
+import os
+import argparse
+from uuid import uuid4
+from Foundation import NSDate
+import FoundationPlist
+
+class PayloadDict:
+    """Class to represent the complete plist contents of a Configuration Profile (.mobileconfig) file"""
+    def __init__(self, identifier, removal_allowed, organization):
+        self.data = {}
+        self.data['PayloadVersion'] = 1
+        self.data['PayloadOrganization'] = organization
+        self.data['PayloadUUID'] = makeNewUUID()
+        if removal_allowed == 'Always':
+            self.data['PayloadRemovalDisallowed'] = False
+        elif removal_allowed == 'Never':
+            self.data['PayloadRemovalDisallowed'] = True
+        self.data['PayloadType'] = 'Configuration'
+        self.data['PayloadScope'] = 'System'
+        self.data['PayloadDescription'] = "Included custom settings:\n"
+        self.data['PayloadDisplayName'] = "PlistToProfile: "
+        self.data['PayloadIdentifier'] = identifier
+
+        # An empty list for 'sub payloads' that we'll fill later
+        self.data['PayloadContent'] = []
+
+    def addPayload(self, plist_dict, domain, manage):
+        """Add one plist dict contents to the profile's payloads. 'domain' is the preferences domain (ie. com.apple.finder),
+and 'manage' is one of 'Once', 'Often' or 'Always'.
+        """
+        payload_dict = {}
+        # Boilerplate
+        payload_dict['PayloadVersion'] = 1
+        payload_dict['PayloadUUID'] = makeNewUUID()
+        payload_dict['PayloadEnabled'] = True
+        payload_dict['PayloadDisplayName'] = "Settings for %s" % domain
+        payload_dict['PayloadType'] = 'com.apple.ManagedClient.preferences'
+        payload_dict['PayloadIdentifier'] = "%s.%s.alacarte.customsettings.%s" % (
+                                            'PlistToProfile', self.data['PayloadUUID'], payload_dict['PayloadUUID'])
+
+        # Update the top-level descriptive info
+        self.data['PayloadDescription'] += "%s\n" % domain
+        self.data['PayloadDisplayName'] += "%s, " % domain
+
+        # Frequency to apply settings, or 'state'
+        if manage == 'Always':
+            state = 'Forced'
+        else:
+            state = 'Set-Once'
+
+        # Yet another nested dict for the actual contents
+        payload_dict['PayloadContent'] = {}
+        payload_dict['PayloadContent'][domain] = {}
+        payload_dict['PayloadContent'][domain][state] = []
+        payload_dict['PayloadContent'][domain][state].append({})
+        payload_dict['PayloadContent'][domain][state][0]['mcx_preference_settings'] = plist_dict
+        # Add a datestamp if we're managing 'Once'
+        if manage == 'Once':
+            now = NSDate.new()
+            payload_dict['PayloadContent'][domain][state][0]['mcx_data_timestamp'] = now
+        self.data['PayloadContent'].append(payload_dict)
+
+
+def makeNewUUID():
+    return str(uuid4())
+
+
+def getDomainFromPlist(plist_path_or_name):
+    """Assuming the domain is also the name of the plist file, strip the path and the ending '.plist'"""
+    return os.path.basename(plist_path_or_name).split('.plist')[0]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--plist', '-p', action="append", metavar='PATH', required=True,
+        help="""Path to a plist to be added as a profile payload.
+Can be specified multiple times.""")
+    parser.add_argument('--manage', '-m', 
+        action="store", 
+        default="Always", 
+        help="Management frequency - Once, Often or Always. Defaults to Always.")
+    parser.add_argument('--removal-allowed', '-r', 
+        action="store", 
+        default="Never", 
+        help="""Specifies when the profile can be removed. Currently supported options are 'Never' and 'Always', 
+and defaults to 'Never'""")
+    parser.add_argument('--organization', '-g', 
+        action="store", 
+        default="",
+        help="Cosmetic name for the organization deploying the profile.")
+    parser.add_argument('--output', '-o', 
+        action="store", 
+        metavar='PATH', 
+        help="Output path for profile. Defaults to 'identifier.mobileconfig' in the current working directory.")
+    parser.add_argument('--identifier', '-i', 
+        action="store", 
+        required=True,
+        help="""Payload identifier. This is what is used to uniquely identify a profile.
+A profile can be removed using this identifier using the 'profiles' command and the '-R -p' options.""")
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        o.print_usage()
+        sys.exit(-1)
+
+    if args.output:
+        output_file = args.output
+    else:
+        first_domain = getDomainFromPlist(args.plist[0])
+        output_file = os.path.join(os.getcwd(), args.identifier + '.mobileconfig')
+
+    newPayload = PayloadDict(identifier=args.identifier, 
+        removal_allowed=args.removal_allowed,
+        organization=args.organization)
+    for plist_path in args.plist:        
+        source_data = FoundationPlist.readPlist(plist_path)
+        source_domain = getDomainFromPlist(plist_path)
+        newPayload.addPayload(source_data, source_domain, args.manage)
+
+    FoundationPlist.writePlist(newPayload.data, output_file)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-PlistToProfile
-==============
+# PlistToProfile
 
-Convert OS X property lists into Configuration Profiles "Custom Settings" for use with OS X 10.7 or greater
+## Overview
+
+PlistToProfile is a simple command-line utility to assemble OS X property lists (plists) into a single "Custom Settings" Configuration Profile for use with OS X 10.7 or greater. Administrators who would like to move from MCX-based management to Profiles may find this tool useful to speed up the process of migrating and testing. Currently it only supports the "Custom Settings" type, as this seems to be the functional equivalent of key-value domain management in Workgroup Manager.
+
+PlistToProfile requires Python 2.7 or greater, which is standard on OS X Lion. It also makes use of Greg Neagle's FoundationPlist library from the Munki project, which provides native plist support via the PyObjC bridge framework.
+
+## Example usage
+
+Here's an example:
+
+`./PlistToProfile.py --plist /path/to/a/plist --identifier MyApplicationPrefs`
+
+This will create a .mobileconfig file in the same directory, which is equivalent to a "Custom Settings" profile configured in the Profile Manager web application included in Lion Server.
+
+## Once/Often/Always management
+
+One complaint about Profile Manager is that it does not provide a mechanism to choose the "management frequency" of an MCX-set preference, such as "Once" or "Often". It was discovered very recently (again, by Greg Neagle) that it's possible to regain that functionality, by slightly altering the contents of the .mobileconfig file to match how it was defined by MCX in OS X 10.6 and earlier:
+
+- for "Often" behaviour, use the 'Set-Once' key instead of 'Forced' for a domain
+- for "Once" behaviour, do this and set an mcx_data_timestamp alongside the mcx_preference_settings which is an NSDate
+
+PlistToProfile provides the same functionality:
+
+`./PlistToProfile.py --plist /path/to/a/plist --identifier MyApplicationPrefs --manage Often`
+
+## Domains
+
+Plist files used for application preferences are typically named by a reverse-domain format, and end in '.plist'. Currently, PlistToProfile will assume that the name portion of the plist file _is_ the domain to be used by MCX. In other words, application preferences won't function if you use something like `--plist my.orgs.office.2011.prefs.plist`, because it will assemble the profile to use the domain 'my.orgs.office.2011.prefs'. If you have collections of default preferences you would like to manage for various applications and system settings, it's best to store these settings in the properly-named plist files, but store _only_ these settings in the files, and nothing else.
+
+## Payload Identifiers
+
+The only arguments required are at least one plist, and an identifier. The identifier is crucial: it is what is defined in the toplevel payload's PayloadIdentifier key, and is what would be used to identify the profile to remove using `profiles -R -p [identifier]`. Also, if you attempt to install a profile with the same identifier, it will update the existing profile instead of installing another profile. The newer version could have completely different payloads and a different toplevel PayloadUUID, but it will still replace it.
+
+As far as I can tell, two profiles with unique toplevel PayloadIdentifiers but matching toplevel PayloadUUIDs will both install successfully. PlistToProfile will always generate unique UUIDs for the toplevel and nested payloads, but because the PayloadIdentifier has to be paid attention to, it's required to manually specify it. To reduce the possibility of human error, I plan to add an alternate option, ie. `--identifier-from-profile`, that would take a path to a previously-built profile and use its PayloadIdentifier.
+
+## Other functionality
+
+- Multiple plists can be defined (with `--plist` or `-p`) and they will be combined as individual payloads within the Configuration Profile
+- It can be specified whether a profile is allowed to be removed using `--removal-allowed` or `-r`
+- An organization name for the profile can be specified using `--organization` or `-g`
+- A specific output filename for the .mobileconfig file can be specified using `--output` or `-o`
+
+## Note
+
+I really do not have much experience with Configuration Profiles, and this is a rough first pass at a generic tool that I knew would help me understand better how Configuration Profiles actually work. There may well be fundamental design changes in how this tool should work to make it useful for others, so I welcome anyone's feedback/suggestions/pull requests.
+
+Special thanks to Greg Neagle for some very useful intial feedback.
